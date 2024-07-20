2f8ec64a3ba630077c740fd7524e1d1eaafcf485
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 14:37:17 +0100

    feat: add 'Like' and 'Review' entities

---
+++ BiteAlert/Modules/LikeModule/Like.cs
@@ -0,0 +1,13 @@
+﻿using BiteAlert.Modules.CustomerModule;
+
+namespace BiteAlert.Modules.LikeModule;
+
+public class Like
+{
+    public Guid Id { get; set; }
+    public string CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public string ProductId { get; set; }
+    public Product Product { get; set; }
+    public DateTime LikedAt { get; set; }
+}
---
+++ BiteAlert/Modules/ReviewModule/Review.cs
@@ -0,0 +1,15 @@
+﻿using BiteAlert.Modules.CustomerModule;
+
+namespace BiteAlert.Modules.ReviewModule;
+
+public class Review
+{
+    public Guid Id { get; set; }
+    public string CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public string ProductId { get; set; }
+    public Product Product { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+    public DateTime ReviewedAt { get; set; }
+}
