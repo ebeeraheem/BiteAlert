2c8a9ff05c8a4875042b50f24209593b02fc020f
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 13:48:21 +0100

    feat: add application user entity
    Add the application user entity and configure the
    'ApplicationUsers' table in the database context

--- BiteAlert/Infrastructure/Data/ApplicationDbContext.cs
+++ BiteAlert/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BiteAlert.Modules.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BiteAlert.Infrastructure.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {        
     }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
---
+++ BiteAlert/Modules/Authentication/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BiteAlert.Modules.Authentication;
+
+public class ApplicationUser : IdentityUser
+{
+    // Id, Email, EmailConfirmed, PhoneNumber, PhoneNumberConfirmed
+    // and Password are inherited from IdentityUser
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public new string UserName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public string ProfilePictureUrl { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
+}
