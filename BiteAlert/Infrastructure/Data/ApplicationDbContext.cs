08791f4fa365454a59302f37dff25d74ec77c710
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 15:19:12 +0100

    feat: configure entities in the database context
    Configure the Vendor, Customer, Product,
    Like, Review, and Notification entities
    in the application database context

--- BiteAlert/Infrastructure/Data/ApplicationDbContext.cs
+++ BiteAlert/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using BiteAlert.Modules.Authentication;
 using BiteAlert.Modules.CustomerModule;
+using BiteAlert.Modules.LikeModule;
+using BiteAlert.Modules.NotificationModule;
+using BiteAlert.Modules.ProductModule;
+using BiteAlert.Modules.ReviewModule;
 using BiteAlert.Modules.VendorModule;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +16,10 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {        
     }
 
-    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Vendor> Vendors { get; set; }
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Like> Likes { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
 }
