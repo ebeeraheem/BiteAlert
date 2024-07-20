96c11a1ce7ba26c71b1334fa93470ea0f7a62cbf
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 14:54:02 +0100

    feat: add 'Notification' entity to the notification module

---
+++ BiteAlert/Modules/NotificationModule/Notification.cs
@@ -0,0 +1,12 @@
+﻿using BiteAlert.Modules.CustomerModule;
+
+namespace BiteAlert.Modules.NotificationModule;
+
+public class Notification
+{
+    public Guid Id { get; set; }
+    public string CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public string Message { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
