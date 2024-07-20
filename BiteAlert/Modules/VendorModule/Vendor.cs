33fe5abafb2b40a9f1de7c6ca74668d0053cddee
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 13:52:45 +0100

    feat: add 'Vendor' entity in the vendor module

---
+++ BiteAlert/Modules/VendorModule/Vendor.cs
@@ -0,0 +1,10 @@
+﻿using BiteAlert.Modules.Authentication;
+
+namespace BiteAlert.Modules.VendorModule;
+
+public class Vendor : ApplicationUser
+{
+    public string BusinessName { get; set; } = string.Empty;
+    public string BusinessDescription { get; set; } = string.Empty;
+    public string BusinessAddress { get; set; } = string.Empty;
+}
