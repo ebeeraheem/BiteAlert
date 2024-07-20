72b5891b518de9b4d3eedd1a01ac242d9f75911d
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-20 14:30:57 +0100

    feat: add rating to the vendor entity

--- BiteAlert/Modules/VendorModule/Vendor.cs
+++ BiteAlert/Modules/VendorModule/Vendor.cs
@@ -7,4 +7,8 @@ public class Vendor : ApplicationUser
     public string BusinessName { get; set; } = string.Empty;
     public string BusinessDescription { get; set; } = string.Empty;
     public string BusinessAddress { get; set; } = string.Empty;
+    //public ICollection<Product> Products { get; set; }
+
+    // The vendor rating is the overall rating of a vendor computed from product ratings
+    //public double OverallRating => Reviews.Any() ? Reviews.Average(r => r.Rating) : 0;
 }
