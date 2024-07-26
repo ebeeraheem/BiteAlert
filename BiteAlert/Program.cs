7f7f2893958785dc6098d77abf4d8c8a0622e32e
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-26 20:16:42 +0100

    refactor: move swagger configurations to 'SwaggerConfig.cs'

--- BiteAlert/Program.cs
+++ BiteAlert/Program.cs
@@ -5,14 +5,8 @@ using BiteAlert.Modules.ProductModule;
 using BiteAlert.Modules.Utilities;
 using BiteAlert.Modules.VendorModule;
 using BiteAlert.StartupConfigs;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,62 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-// Get swagger config values from appsettings.json
-var title = builder.Configuration.GetSection("SwaggerDoc:Title").Value;
-var description = builder.Configuration.GetSection("SwaggerDoc:Description").Value;
-var name = builder.Configuration.GetSection("SwaggerDoc:Contact:Name").Value;
-var email = builder.Configuration.GetSection("SwaggerDoc:Contact:Email").Value;
-var url = builder.Configuration.GetSection("SwaggerDoc:Contact:Url").Value;
-
-builder.Services.AddSwaggerGen(options =>
-{
-    // Add the API info and description
-    options.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Version = "v1",
-        Title = title,
-        Description = description,
-        Contact = new OpenApiContact()
-        {
-            Name = name,
-            Email = email,
-            Url = new Uri(url!)
-        }
-    });
-
-    // Add the authorize button in the SwaggerUI
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Type = SecuritySchemeType.Http,
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Description = "Enter your token here:",
-    });
-
-    // Add the lock icon to all endpoints in SwaggerUI
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer",
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-
-    // Add XML comments in SwaggerUI
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-
-    options.IncludeXmlComments(xmlPath);
-});
+builder.AddSwaggerConfigurations();
 
 // Configure database connection string
 builder.Services.AddDbContext<ApplicationDbContext>(
---
+++ BiteAlert/StartupConfigs/SwaggerConfig.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace BiteAlert.StartupConfigs;
+
+public static class SwaggerConfig
+{
+    public static void AddSwaggerConfigurations(this WebApplicationBuilder builder)
+    {
+        // Get swagger config values from appsettings.json
+        var title = builder.Configuration.GetSection("SwaggerDoc:Title").Value;
+        var description = builder.Configuration.GetSection("SwaggerDoc:Description").Value;
+        var name = builder.Configuration.GetSection("SwaggerDoc:Contact:Name").Value;
+        var email = builder.Configuration.GetSection("SwaggerDoc:Contact:Email").Value;
+        var url = builder.Configuration.GetSection("SwaggerDoc:Contact:Url").Value;
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            // Add the API info and description
+            options.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = title,
+                Description = description,
+                Contact = new OpenApiContact()
+                {
+                    Name = name,
+                    Email = email,
+                    Url = new Uri(url!)
+                }
+            });
+
+            // Add the authorize button in the SwaggerUI
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Type = SecuritySchemeType.Http,
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Description = "Enter your token here:",
+            });
+
+            // Add the lock icon to all endpoints in SwaggerUI
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+            // Add XML comments in SwaggerUI
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+            options.IncludeXmlComments(xmlPath);
+        });
+    }
+}
