e1d2966533ccb4dc761c8d10915449513421b256
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-26 20:11:10 +0100

    refactor: move authentication logic to JwtAuthentication.cs

--- BiteAlert/Program.cs
+++ BiteAlert/Program.cs
@@ -99,45 +99,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// Retrieve JWT configuration values
-var jwtIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer");
-var jwtAudience = builder.Configuration.GetValue<string>("Jwt:Audience");
-var jwtKey = builder.Configuration.GetValue<string>("Jwt:Key");
-
-// Validate JWT configuration values
-if (string.IsNullOrWhiteSpace(jwtIssuer) ||
-    string.IsNullOrWhiteSpace(jwtAudience) ||
-    string.IsNullOrWhiteSpace(jwtKey))
-{
-    throw new InvalidOperationException("JWT configuration values are missing.");
-}
-
-// Configure JWT authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtIssuer,
-            ValidAudience = jwtAudience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                jwtKey))
-        };
-    });
-
-// Require users of the app to be authenticated
-builder.Services.AddAuthorizationBuilder()
-    .SetFallbackPolicy(new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build());
+builder.AddJwtAuthentication();
 
 var app = builder.Build();
 
---
+++ BiteAlert/StartupConfigs/JwtAuthentication.cs
@@ -0,0 +1,54 @@
+﻿// Ignore Spelling: Jwt
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BiteAlert.StartupConfigs;
+
+public static class JwtAuthentication
+{
+    public static void AddJwtAuthentication(this WebApplicationBuilder builder)
+    {
+        // Retrieve JWT configuration values
+        var jwtIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer");
+        var jwtAudience = builder.Configuration.GetValue<string>("Jwt:Audience");
+        var jwtKey = builder.Configuration.GetValue<string>("Jwt:Key");
+
+        // Validate JWT configuration values
+        if (string.IsNullOrWhiteSpace(jwtIssuer) ||
+            string.IsNullOrWhiteSpace(jwtAudience) ||
+            string.IsNullOrWhiteSpace(jwtKey))
+        {
+            throw new InvalidOperationException("JWT configuration values are missing.");
+        }
+
+        // Configure JWT authentication
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                        jwtKey))
+                };
+            });
+
+        // Require users of the app to be authenticated
+        builder.Services.AddAuthorizationBuilder()
+            .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build());
+    }
+}
