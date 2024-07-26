810287404a0cef6f35e1d4c556dfd1dd692dab78
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-26 16:58:43 +0100

    feat: configure swagger to use authentication

--- BiteAlert/Program.cs
+++ BiteAlert/Program.cs
@@ -6,13 +6,61 @@ using BiteAlert.Modules.Utilities;
 using BiteAlert.Modules.VendorModule;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Get swagger config values from appsettings.json
+var title = builder.Configuration.GetSection("SwaggerDoc:Title").Value;
+var description = builder.Configuration.GetSection("SwaggerDoc:Description").Value;
+var name = builder.Configuration.GetSection("SwaggerDoc:Contact:Name").Value;
+var email = builder.Configuration.GetSection("SwaggerDoc:Contact:Email").Value;
+var url = builder.Configuration.GetSection("SwaggerDoc:Contact:Url").Value;
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = title,
+        Description = description,
+        Contact = new OpenApiContact()
+        {
+            Name = name,
+            Email = email,
+            Url = new Uri(url!)
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme.",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(
