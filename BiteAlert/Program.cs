39e603a3a9d69bd9a6ec77ff99ff4562a30cdb10
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-26 20:21:22 +0100

    refactor: move custom services to 'CustomServices.cs'

--- BiteAlert/Program.cs
+++ BiteAlert/Program.cs
@@ -1,9 +1,4 @@
 using BiteAlert.Infrastructure.Data;
-using BiteAlert.Modules.Authentication;
-using BiteAlert.Modules.CustomerModule;
-using BiteAlert.Modules.ProductModule;
-using BiteAlert.Modules.Utilities;
-using BiteAlert.Modules.VendorModule;
 using BiteAlert.StartupConfigs;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,23 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.AddSwaggerConfigurations();
-
 // Configure database connection string
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add custom services
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IVendorService, VendorService>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<UserContextService>();
-
-
-
+builder.AddSwaggerConfigurations();
 builder.AddJwtAuthentication();
+builder.AddCustomServices();
 
 var app = builder.Build();
 
---
+++ BiteAlert/StartupConfigs/CustomServices.cs
@@ -0,0 +1,20 @@
+﻿using BiteAlert.Modules.Authentication;
+using BiteAlert.Modules.CustomerModule;
+using BiteAlert.Modules.ProductModule;
+using BiteAlert.Modules.Utilities;
+using BiteAlert.Modules.VendorModule;
+
+namespace BiteAlert.StartupConfigs;
+
+public static class CustomServices
+{
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        // Add custom services
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IVendorService, VendorService>();
+        builder.Services.AddScoped<ICustomerService, CustomerService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<UserContextService>();
+    }
+}
