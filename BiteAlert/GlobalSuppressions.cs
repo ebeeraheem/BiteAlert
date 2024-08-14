ed322e95340b2ca13a2af54420b9284a4201d7ad
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-08-14 20:11:59 +0100

    feat: add global suppressions file

---
+++ BiteAlert/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.Authentication.UserService.LoginUserAsync(BiteAlert.Modules.Authentication.LoginUserRequest)~System.Threading.Tasks.Task{BiteAlert.Modules.Authentication.LoginUserResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.Authentication.UserService.RegisterUserAsync(BiteAlert.Modules.Authentication.RegisterUserRequest)~System.Threading.Tasks.Task{BiteAlert.Modules.Authentication.RegisterUserResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.Authentication.UserService.UpdatePasswordAsync(System.String,BiteAlert.Modules.Authentication.UpdatePasswordRequest)~System.Threading.Tasks.Task{BiteAlert.Modules.Authentication.UserProfileResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.Authentication.UserService.UpdateProfileAsync(System.String,BiteAlert.Modules.Authentication.UserProfileRequest)~System.Threading.Tasks.Task{BiteAlert.Modules.Authentication.UserProfileResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.ProductModule.ProductService.CreateAsync(System.String,BiteAlert.Modules.ProductModule.UpsertProductRequest)~System.Threading.Tasks.Task{BiteAlert.Modules.ProductModule.UpsertProductResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.Modules.ProductModule.ProductService.GetProductByIdAsync(System.String)~System.Threading.Tasks.Task{BiteAlert.Modules.ProductModule.UpsertProductResponse}")]
+[assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:BiteAlert.StartupConfigs.Seeder.SeedRoles(System.IServiceProvider)~System.Threading.Tasks.Task")]
