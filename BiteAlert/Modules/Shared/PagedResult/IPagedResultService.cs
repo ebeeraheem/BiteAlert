cb9fe0bc5e199e8aa84a955d50e04693974d3cac
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-09-07 17:05:59 +0100

    feat(Shared): add paged result service

---
+++ BiteAlert/Modules/Shared/PagedResult/IPagedResultService.cs
@@ -0,0 +1,10 @@
+﻿namespace BiteAlert.Modules.Shared.PagedResult;
+
+public interface IPagedResultService
+{
+    Task<PagedResult<T>> GetPagedResultAsync<T>(
+       IQueryable<T> query,
+       int pageNumber,
+       int pageSize)
+       where T : class;
+}
